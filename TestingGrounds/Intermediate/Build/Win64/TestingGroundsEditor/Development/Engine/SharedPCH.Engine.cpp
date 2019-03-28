e9d57c22039487dd4b784eddbce4a96a135f201a
mszynawa <m.szynawa@yahoo.com>
2019-03-28 16:40:53 +0100

    [TestingGrounds] Inicjalizacja projektu. Wgranie podstawowych assetów. Ruch NPC pomiędzy dwoma punktami z użyciem behavior tree BP.

---
+++ TestingGrounds/Intermediate/Build/Win64/TestingGroundsEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "D:/[GIT] Repositories/UnrealEngine-Games/UnrealEngine-Games/TestingGrounds/Intermediate/Build/Win64/TestingGroundsEditor/Development/Engine/SharedPCH.Engine.h"
---
+++ TestingGrounds/Intermediate/Build/Win64/UE4Editor/Inc/TestingGrounds/TestingGroundsGameMode.generated.h
@@ -0,0 +1,88 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef TESTINGGROUNDS_TestingGroundsGameMode_generated_h
+#error "TestingGroundsGameMode.generated.h already included, missing '#pragma once' in TestingGroundsGameMode.h"
+#endif
+#define TESTINGGROUNDS_TestingGroundsGameMode_generated_h
+
+#define TestingGrounds_Source_TestingGrounds_TestingGroundsGameMode_h_12_RPC_WRAPPERS
+#define TestingGrounds_Source_TestingGrounds_TestingGroundsGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define TestingGrounds_Source_TestingGrounds_TestingGroundsGameMode_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesATestingGroundsGameMode(); \
+	friend struct Z_Construct_UClass_ATestingGroundsGameMode_Statics; \
+public: \
+	DECLARE_CLASS(ATestingGroundsGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/TestingGrounds"), TESTINGGROUNDS_API) \
+	DECLARE_SERIALIZER(ATestingGroundsGameMode)
+
+
+#define TestingGrounds_Source_TestingGrounds_TestingGroundsGameMode_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesATestingGroundsGameMode(); \
+	friend struct Z_Construct_UClass_ATestingGroundsGameMode_Statics; \
+public: \
+	DECLARE_CLASS(ATestingGroundsGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/TestingGrounds"), TESTINGGROUNDS_API) \
+	DECLARE_SERIALIZER(ATestingGroundsGameMode)
+
+
+#define TestingGrounds_Source_TestingGrounds_TestingGroundsGameMode_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	TESTINGGROUNDS_API ATestingGroundsGameMode(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ATestingGroundsGameMode) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(TESTINGGROUNDS_API, ATestingGroundsGameMode); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATestingGroundsGameMode); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	TESTINGGROUNDS_API ATestingGroundsGameMode(ATestingGroundsGameMode&&); \
+	TESTINGGROUNDS_API ATestingGroundsGameMode(const ATestingGroundsGameMode&); \
+public:
+
+
+#define TestingGrounds_Source_TestingGrounds_TestingGroundsGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	TESTINGGROUNDS_API ATestingGroundsGameMode(ATestingGroundsGameMode&&); \
+	TESTINGGROUNDS_API ATestingGroundsGameMode(const ATestingGroundsGameMode&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(TESTINGGROUNDS_API, ATestingGroundsGameMode); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATestingGroundsGameMode); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ATestingGroundsGameMode)
+
+
+#define TestingGrounds_Source_TestingGrounds_TestingGroundsGameMode_h_12_PRIVATE_PROPERTY_OFFSET
+#define TestingGrounds_Source_TestingGrounds_TestingGroundsGameMode_h_9_PROLOG
+#define TestingGrounds_Source_TestingGrounds_TestingGroundsGameMode_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TestingGrounds_Source_TestingGrounds_TestingGroundsGameMode_h_12_PRIVATE_PROPERTY_OFFSET \
+	TestingGrounds_Source_TestingGrounds_TestingGroundsGameMode_h_12_RPC_WRAPPERS \
+	TestingGrounds_Source_TestingGrounds_TestingGroundsGameMode_h_12_INCLASS \
+	TestingGrounds_Source_TestingGrounds_TestingGroundsGameMode_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define TestingGrounds_Source_TestingGrounds_TestingGroundsGameMode_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TestingGrounds_Source_TestingGrounds_TestingGroundsGameMode_h_12_PRIVATE_PROPERTY_OFFSET \
+	TestingGrounds_Source_TestingGrounds_TestingGroundsGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	TestingGrounds_Source_TestingGrounds_TestingGroundsGameMode_h_12_INCLASS_NO_PURE_DECLS \
+	TestingGrounds_Source_TestingGrounds_TestingGroundsGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID TestingGrounds_Source_TestingGrounds_TestingGroundsGameMode_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
